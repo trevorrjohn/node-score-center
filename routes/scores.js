b50854c57a232f06add7381ad1c5720ea4f8e8f3
Trevor John, tjohn <pair+trevor@john.tj>
2013-04-10 09:54:04 -0400

    Clean up error handling

--- routes/scores.js
+++ routes/scores.js
@@ -1,8 +1,9 @@
 var mongo = require('mongodb');
 
 var mongoUri = process.env.MONGOHQ_URL || 'mongodb://localhost/scorecenter';
+
 var db = mongo.Db.connect(mongoUri, function (err, dbConnection) {
-  if (err) { process.stderr.write("Error connecting to database: " + err + "\n") }
+  if (err) { outputError("Error connecting to database: " + err + "\n") }
   else db = dbConnection;
 })
 
@@ -15,16 +16,19 @@ exports.create = function (req, res) {
   score = parseInt(req.body.score);
 
   if ( game_title && username && score ) {
-    db.collection('highscores', function (err, collection) {
-      if (err) { process.stderr.write("Error creating highscore" + err + "\n") }
-      else {
+    db.collection('highscores', function (err, highscores) {
+      if (err) {
+        writeError(err, req)
+      } else {
         var new_score = { game_title: game_title, username: username, score: score, created_at: new Date };
 
-        collection.insert( new_score, { safe: true }, function (err, result) {
+        highscores.insert( new_score, { safe: true }, function (err, result) {
           if (err) {
-            process.stderr.write("Error inserting new highscore: " + err + "\n")
+            writeError(err, req)
             res.send({ error: 'An error has occurred' })
-          } else res.send( result[0] )
+          } else {
+            res.send( result[0] )
+          }
         })
       }
     })
@@ -34,12 +38,16 @@ exports.create = function (req, res) {
 }
 
 exports.index = function (req, res) {
-  db.collection('highscores', function(err, collection) {
-    if (err) { process.stderr.write( "Error in collection in index: " + err + "\n") }
-    else {
-      collection.find().toArray( function (err, items) {
-        if (err) { process.stderr.write("Error in find in index: " + err + "\n") }
-        else res.render('index', { title: 'Scores', scores: items })
+  db.collection('highscores', function(err, highscores) {
+    if (err) {
+      writeError(err, req)
+    } else {
+      highscores.find().toArray( function (err, items) {
+        if (err) {
+          writeError(err, req)
+        } else {
+          res.render('index', { title: 'Scores', scores: items })
+        }
       })
     }
   })
@@ -48,12 +56,19 @@ exports.index = function (req, res) {
 exports.highscores = function (req, res) {
   var game_title = req.query.game_title
   if (game_title) {
-    db.collection('highscores', function(err, collection) {
-      if (err) { process.stderr.write( "Error in collection in all: " + err + "\n") }
-      else {
-        collection.find({ game_title: game_title }).sort({ score: -1 }).limit(10).toArray( function (err, items) {
-          if (err) { process.stderr.write("Error in highscores in find: " + err + "\n") }
-          else res.send(items)
+    db.collection('highscores', function(err, highscores) {
+      if (err) {
+        writeError(err, req)
+      } else {
+        highscores.find({ game_title: game_title }).
+                   sort({ score: -1 }).
+                   limit(10).
+                   toArray( function (err, items) {
+                     if (err) {
+                       writeError(err, req)
+                     } else {
+                       res.send(items)
+                     }
         })
       }
     })
@@ -69,16 +84,39 @@ exports.usersearch = function (req, res) {
 exports.search = function (req, res) {
   var username = req.body.username
   if (username) {
-    db.collection('highscores', function(err, collection) {
-      if (err) { process.stderr.write("Error in search in collection: " + err + "\n") }
-      else {
-        collection.find({ username: username }).toArray( function (err, items) {
-          if (err) { process.stderr.write("Error in search in find: " + err + "\n") }
-          else res.send({ status: "Complete", scores: items })
+    db.collection('highscores', function(err, highscores) {
+      if (err) {
+        writeError(err, req)
+      } else {
+        highscores.find({ username: username }).toArray( function (err, items) {
+          if (err) {
+            writeError(err, req)
+          } else {
+            res.send({ status: "Complete", scores: items })
+          }
         })
       }
     })
   } else {
     res.send({scores: [], status: "No 'username' found in body."})
+  }
+}
+
+/* Error handling */
+
+var writeError = function (error, req) {
+  outputError(error + "\n")
+  outputError(req.route + "\n")
+  if (req.query.game_title)
+    outputError(req.query + "\n")
+  if (req.body.game_title || req.body.username || req.body.score)
+    outputError(req.body + "\n")
+}
+
+var outputError = function (msg) {
+  if (process.env.ENV == 'production') {
+    process.stderr.write(msg)
+  } else {
+    console.log(msg)
   }
 }
